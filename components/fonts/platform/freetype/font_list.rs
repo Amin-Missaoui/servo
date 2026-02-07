d9002e50fbf3b93bb1263c995860bb0f6c5b83ad
Martin Robinson <mrobinson@igalia.com>
2026-02-07 20:20:47 +0100

    layout: Store a fallback cache in the `FontGroup`
    Store a list of recently used fallbacks in the `FontGroups`, keyed by
    `lang` property and the detected `UnicodeBlock` and `Script` of the
    character in question. This should mean that less work is done when
    switching between falling back and not falling back. In addition, this
    will allow better caching of `FontRef`s that come directly from the
    platform font backend -- something necessary for the next generation
    font fallback code.
    
    Signed-off-by: Martin Robinson <mrobinson@igalia.com>

--- components/fonts/platform/freetype/font_list.rs
+++ components/fonts/platform/freetype/font_list.rs
@@ -188,13 +188,13 @@ pub fn fallback_font_families(options: FallbackFontSelectionOptions) -> Vec<&'st
             // In Japanese typography, it is not common to use different fonts
             // for Kanji(Han), Hiragana, and Katakana within the same document.
             // We uniformly fallback to Japanese fonts when the document language is Japanese.
-            _ if options.lang == Some(String::from("ja")) => {
+            _ if options.lang == XLang("ja".into()) => {
                 families.push("TakaoPGothic");
             },
             _ if matches!(
                 Script::from(options.character),
                 Script::Bopomofo | Script::Han
-            ) && options.lang != Some(String::from("ja")) =>
+            ) && options.lang != XLang("ja".into()) =>
             {
                 families.push("WenQuanYi Micro Hei");
             },
